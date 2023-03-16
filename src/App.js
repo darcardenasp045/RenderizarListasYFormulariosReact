308188690874ac32cfc2d4018ee6209da34e49bd
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2023-03-16 14:06:52 -0500

    se agrago un objeto dentro de otro objeto, de esta manera revisamos como renderizarlos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./styles.css";
 const notas = [
   {
     id: 25,
-    date: 20
+    date: 20,
+    object: {
+      hola: "hyyy",
+      idd: 25
+    }
   }
 ];
 
@@ -11,19 +15,25 @@ export default function App() {
   if (typeof notas === undefined || notas === null) {
     return "no hay notas";
   }
-  const Note = ({ note }) => {
+  const Note = ({ id, date, object }) => {
     return (
-      <p>
-        {" "}
-        {note.id}
-        {note.date}
-      </p>
+      <div>
+        <p>
+          {" "}
+          {id}
+          {date}
+        </p>
+        <p>
+          {object.hola}
+          {object.idd}
+        </p>
+      </div>
     );
   };
   return (
     <div className="App">
       {notas.map((note) => {
-        return <Note note={note} />;
+        return <Note id={note.id} date={note.date} object={note.object} />;
       })}
     </div>
   );
